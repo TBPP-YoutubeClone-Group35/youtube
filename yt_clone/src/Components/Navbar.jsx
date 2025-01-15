86b9d05ea2e27c1fefdadfdc19cef50e7b770e9a
my <yash.sharma83076@gmail.com>
2025-01-15 22:41:50 +0530

    nabar, front page edit little bit

--- yt_clone/src/Components/Navbar.jsx
+++ yt_clone/src/Components/Navbar.jsx
@@ -1,7 +1,25 @@
-import React from React;
+import React from 'react';
+// Import a CSS file for styling
 
 const Navbar = () => {
-    return <div classname="App"></div>
-}
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img
+          src=""
+          alt="YouTube Logo"
+          className="logo"
+        />
+      </div>
+      <div className="navbar-search">
+        <input
+          type="text"
+          placeholder="Search"
+          className="search-input"
+        />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
